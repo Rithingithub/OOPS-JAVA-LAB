9f725d42fb2489cecee0ad5697f225447a0e8f63
Rithin Lehan <74766580+Rithingithub@users.noreply.github.com>
2023-02-19 21:39:28 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class palindrome{
+    public static void main(String args[])
+    {
+String str,rev="";
+Scanner Sc=new Scanner(System.in);
+System.out.println("Enter a string:");
+str=Sc.nextLine();
+int length=str.length();
+for(int i=length-1;i>=0;i--)
+rev=rev+str.charAt(i);
+if(str.equals(rev))
+System.out.println(str+"  is a palindrome");
+else
+System.out.println(str+"  is not a palindrome");
+    }
+}
